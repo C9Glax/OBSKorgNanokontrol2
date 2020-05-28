52af6d7cc4e6c297a1ae50cd9904a8cd6873f544
Glax <gfreak70@gmail.com>
2020-05-28 17:39:20 +0200

    Added Fancy Logging

--- OBSMidi/ConsoleExecutable/Executable.cs
+++ OBSMidi/ConsoleExecutable/Executable.cs
@@ -9,12 +9,42 @@ namespace ConsoleExecutable
         public Executable(string url, string password)
         {
             Kontrol2OBS control = new Kontrol2OBS(url, password);
-            control.OnStatusLog += (s, e) => { Console.WriteLine(e.text); };
+            control.OnStatusLog += (s, e) => { this.WriteLine(LogType.Status, e.text); };
+            control.OnWarningLog += (s, e) => { this.WriteLine(LogType.Warning, e.text); };
+            control.OnInfoLog += (s, e) => { this.WriteLine(LogType.Information, e.text); };
             control.Create();
 
-            Console.WriteLine("Press Escape for clean shutdown.");
+            this.WriteLine(LogType.Information, "Press Escape for clean shutdown.");
             while (Console.ReadKey().Key != ConsoleKey.Escape) ;
             control.Dispose();
+        }
+
+        private enum LogType { Information, Warning, Status}
+        private void WriteLine(LogType logtype, string text)
+        {
+            DateTime logtime = DateTime.Now;
+            string time = string.Format("{0}:{1}:{2}.{3}", logtime.Hour, logtime.Minute, logtime.Second, logtime.Millisecond);
+            string type;
+            switch (logtype)
+            {
+                case LogType.Information:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    type = "Info";
+                    break;
+                case LogType.Warning:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    type = "WARN";
+                    break;
+                case LogType.Status:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    type = "Status";
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    type = "";
+                    break;
+            }
+            Console.WriteLine("[{0}] ({1}) {2}", time, type, text);
         }
 
         static void Main(string[] args)
