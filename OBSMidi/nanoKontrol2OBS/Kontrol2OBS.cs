9ba46235f999c69f1fe49a3780e6dcacd19b1c6c
Glax <gfreak70@gmail.com>
2021-01-02 22:38:08 +0100

    Cleanup of some messy code and added comments

--- OBSMidi/nanoKontrol2OBS/Kontrol2OBS.cs
+++ OBSMidi/nanoKontrol2OBS/Kontrol2OBS.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Main Class and entry point
+ * TrayExecutable and ConsoleExecutable both call here
+ */
+
+using System;
 using System.Collections.Generic;
 using MidiAccess;
 using WindowsSoundControl;
@@ -10,6 +15,10 @@ namespace nanoKontrol2OBS
 {
     static class ExtensionMethod
     {
+        /*
+         * How is this not a standard-method...
+         * Maps a linear range to a smaller/larger one
+         */
         public static double Map(this double value, double low1, double high1, double low2, double high2)
         {
             return (value - low1) / (high1 - low1) * (high2 - low2) + low2;
@@ -25,24 +34,33 @@ namespace nanoKontrol2OBS
         private Controller nanoController;
         private Dictionary<SpecialSourceType, SpecialSourceObject> specialSources;
         private Scene[] obsScenes;
-        private readonly string url, password;
+        private event LogEventHandler OnInfoLog, OnWarningLog, OnStatusLog;
 
-        public Kontrol2OBS(string url, string password)
+        /*
+         * Complete Setup
+         * url: location of obs-websocket (ip+port)
+         * password: password
+         * statusCallback: Method to call in case of a Status-Update
+         * warningCallback: Method to call in case of a Warning-Message
+         * infoCallback: Method to call in case of a Info-Message
+         */
+        public Kontrol2OBS(string url, string password, LogEventHandler statusCallback, LogEventHandler warningCallback, LogEventHandler infoCallback)
         {
-            this.url = url;
-            this.password = password;
-        }
+            this.OnStatusLog = statusCallback;
+            this.OnWarningLog = warningCallback;
+            this.OnInfoLog = infoCallback;
 
-        public void Create()
-        {
             this.UpdateLogStatus("Loading Bindings...");
             this.bindingConfig = new Config(this, @".\config.xml");
+
             this.UpdateLogStatus("Connecting to websocket...");
             this.obsSocket = new OBSConnector(url, password);
             this.obsSocket.OnOBSWebsocketInfo += (s, e) => { this.OnInfoLog?.Invoke(s, new LogEventArgs() { text = e.text }); };
             this.obsSocket.OnOBSWebsocketWarning += (s, e) => { this.OnWarningLog?.Invoke(s, new LogEventArgs() { text = e.text }); };
+
             this.UpdateLogStatus("Setting up audio (This might take a while)...");
             this.SetupAudio();
+
             this.UpdateLogStatus("Connecting nanoKontrol2...");
             this.nanoController = new Controller(GetNanoKontrolInputDeviceName(), GetNanoKontrolOutputDeviceName());
 
@@ -58,35 +76,56 @@ namespace nanoKontrol2OBS
 
             this.nanoController.OnMidiMessageReceived += OnNanoControllerInput;
             this.SetupNanoController();
+
             this.UpdateLogStatus("Setup Event Handlers...");
             this.SetupOBSEventHandlers();
+
             this.UpdateLogStatus("Connected and Ready!");
         }
 
+        /*
+         * Tries to exit gracefully...
+         * And then just kills it
+         */
         public void Dispose()
         {
             this.UpdateLogStatus("Disposing...");
+
+            //Disconnect nanoKontrol2
             if(this.nanoController != null)
             {
                 for (byte cc = 16; cc < 70; cc++)
                     this.nanoController.ToggleLED(cc, false);
                 this.nanoController.Dispose();
             }
+
+            //Disconnect from WebSocket
             if(this.obsSocket != null)
                 this.obsSocket.Close();
+
+            //Disconnect AudioDevices
             foreach (SpecialSourceObject specialSource in this.specialSources.Values)
                 if(specialSource.connected && specialSource.windowsDevice != null)
                     specialSource.windowsDevice.Dispose();
+
             this.UpdateLogStatus("Finished. Goodbye!");
-            System.Environment.Exit(0);
+            Environment.Exit(0);
         }
 
+        /*
+         * WebSocket received a Stream-Status-Update
+         * Turn on/off specified LED
+         */
         private void OnStreamStatusUpdate(object sender, OBSConnector.StreamStatusUpdateEventArgs e)
         {
             this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.streamstatuschanged), e.streaming);
             this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.replaystatuschanged), e.replayBufferActive);
         }
 
+        /*
+         * WebSocket received a Scene-Update
+         * Turn on/off specified LED
+         */
         private void OnSceneSwitched(object sender, OBSConnector.SceneSwitchedEventArgs e)
         {
             string currentScene = e.newSceneName;
@@ -99,6 +138,10 @@ namespace nanoKontrol2OBS
             }
         }
 
+        /*
+         * WebSocket received a Source-Mute-Update
+         * Turn on/off specified LED
+         */
         private void OnOBSSourceMuteChanged(object sender, OBSConnector.SourceMuteChangedEventArgs e)
         {
             foreach (SpecialSourceObject potentialSender in this.specialSources.Values)
@@ -117,7 +160,11 @@ namespace nanoKontrol2OBS
                     break;
                 }
         }
-        
+
+        /*
+         * Audio Device was un-/muted
+         * Turn on/off specified LED
+         */
         private void WindowsDevice_OnMuteStateChanged(object sender, AudioDevice.OnMuteStateChangedEventArgs e)
         {
             foreach (SpecialSourceObject potentialSender in this.specialSources.Values)
@@ -137,6 +184,10 @@ namespace nanoKontrol2OBS
                 }
         }
 
+        /*
+         * MIDI-Event was received
+         * Perform action as set in config.xml
+         */
         private void OnNanoControllerInput(object sender, Controller.MidiMessageReceivedEventArgs e)
         {
             if (this.bindingConfig.inputbindings.ContainsKey(e.control))
@@ -184,13 +235,18 @@ namespace nanoKontrol2OBS
             }
         }
 
+        /*
+         * Handles Initial Setup of AudioDevices
+         * Connects OBS-AudioDevices with Windows-Devices (for this application)
+         */
         private void SetupAudio()
         {
-            SpecialSources obsSpecialSources = this.obsSocket.GetSpecialSources();
+            SpecialSources obsSpecialSources = this.obsSocket.GetSpecialSources(); //Not very special actually...
             List<string> connectedSpecialSources = new List<string>();
             foreach (Source s in this.obsSocket.GetSourcesList())
                 if (s.typeId.Contains("wasapi"))
                     connectedSpecialSources.Add(s.name);
+
             this.specialSources = new Dictionary<SpecialSourceType, SpecialSourceObject>()
             {
                 {
@@ -222,22 +278,16 @@ namespace nanoKontrol2OBS
                 if (specialSource.connected)
                 {
                     string pid = this.obsSocket.GetPIDOfAudioDevice(specialSource.obsSourceName);
-                    if(pid != "default")
+                    if(pid != "default") //OBS-"default"-assigned devices-PID is not transmitted by WebSocket. So we cant actually control their volume...
                     {
-                        string guid = pid.Replace("}.{", "@").Split('@')[1].Substring(0, 36);
-                        try
+                        string guid = pid.Replace("}.{", "@").Split('@')[1].Substring(0, 36); //Convert the PID to GUID Format
+                        try //If the device is not actually connected
                         {
                             specialSource.windowsDevice = new AudioDevice(guid);
                             specialSource.windowsDevice.OnMuteStateChanged += WindowsDevice_OnMuteStateChanged;
                         }
-                        catch(Exception e)
-                        {
-                            this.LogWarning(e.Message);
-                        }
-                        finally
-                        {
-                            this.Dispose();
-                        }
+                        catch(Exception e) { this.LogWarning(e.Message); }
+                        finally { this.Dispose(); }
                     }
                     else
                     {
@@ -247,8 +297,14 @@ namespace nanoKontrol2OBS
             }
         }
 
+        /*
+         * Handles initial Setup of the nanoKontrol2
+         */
         private void SetupNanoController()
         {
+            /*
+             * Toggle LEDs on/off if OBS-Scene is active
+             */
             this.obsScenes = this.obsSocket.GetSceneList().scenes;
             string currentScene = this.obsSocket.GetCurrentScene().name;
             for (byte soloButtonIndex = 0; soloButtonIndex < this.obsScenes.Length && soloButtonIndex < 8; soloButtonIndex++)
@@ -259,6 +315,10 @@ namespace nanoKontrol2OBS
                     this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.sceneswitched), false);
             }
 
+            /*
+             * Toggle LEDs on/off if OBS-Audio is un-/muted
+             * Read at own risk.
+             */
 #pragma warning disable IDE0075
             this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.obsmutechanged, SpecialSourceType.desktop1),this.specialSources[SpecialSourceType.desktop1].connected ? !this.obsSocket.GetMute(this.specialSources[SpecialSourceType.desktop1].obsSourceName) : false);
             this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.obsmutechanged, SpecialSourceType.desktop2), this.specialSources[SpecialSourceType.desktop2].connected ? !this.obsSocket.GetMute(this.specialSources[SpecialSourceType.desktop2].obsSourceName) : false);
@@ -273,10 +333,16 @@ namespace nanoKontrol2OBS
             this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.windowsmutechanged, SpecialSourceType.mic3), this.specialSources[SpecialSourceType.mic3].connected && this.specialSources[SpecialSourceType.mic3].windowsDevice != null ? this.specialSources[SpecialSourceType.mic3].windowsDevice.IsMuted() : false);
 #pragma warning restore IDE0075
 
+            /*
+             * Toggle LED on/off if stream is in-/active
+             */
             GetStreamingStatusObject stats = this.obsSocket.GetStreamingStatus();
             this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.streamstatuschanged), stats.streaming);
         }
 
+        /*
+         * Handles LED-toggeling in case of OBS-Event (WebSocket)
+         */
         private void SetupOBSEventHandlers()
         {
             this.obsSocket.OnStreamStarted += (s, e) => { this.nanoController.ToggleLED(this.bindingConfig.GetOutputToEvent(Config.outputevent.streamstatuschanged), true); };
@@ -309,27 +375,38 @@ namespace nanoKontrol2OBS
                 this.LogWarning("Device: {0}", notNanoKontrol);
             return string.Empty;
         }
-
-        public event LogEventHandler OnInfoLog;
-        public event LogEventHandler OnWarningLog;
-        public event LogEventHandler OnStatusLog;
         
+        /*
+         * Custom EventHandler
+         */
         public delegate void LogEventHandler(object sender, LogEventArgs e);
         public class LogEventArgs : EventArgs
         {
             public string text;
         }
 
+        /*
+         * Invokes a Warning-Message
+         * Also takes care of formatting
+         */
         public void LogWarning(string format, params string[] replace)
         {
             this.OnWarningLog?.Invoke(this, new LogEventArgs() { text = string.Format(format, replace) });
         }
 
+        /*
+         * Invokes a Info-Message
+         * Also takes care of formatting
+         */
         public void LogInfo(string format, params string[] replace)
         {
             this.OnInfoLog?.Invoke(this, new LogEventArgs() { text = string.Format(format, replace) });
         }
 
+        /*
+         * Invokes a Status-Update
+         * Also takes care of formatting
+         */
         public void UpdateLogStatus(string format, params string[] replace)
         {
             this.OnStatusLog?.Invoke(this, new LogEventArgs() { text = string.Format(format, replace) });
